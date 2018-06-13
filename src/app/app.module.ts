88ff360b9d7519025584aec32fee76f81b096158
Hernan Velasquez <hernandvo@gmail.com>
2018-06-12 22:23:53 -0500

    Se agrega ejemplo usando el servicio de https://restcountries.eu/

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {HttpClientModule} from '@angular/common/http';
+
 import {RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -21,6 +23,7 @@ import {ROUTES} from './app.routes';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot(ROUTES, {useHash: true} )
   ],
   providers: [],
